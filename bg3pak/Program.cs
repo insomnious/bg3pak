1b0a2f82ae86403faeabbf63706cc76bcdee21a8
insomnious <simon.davies@nexusmods.com>
2024-09-26 09:06:03 +0100

    refactoring

--- bg3pak/Program.cs
+++ bg3pak/Program.cs
@@ -6,112 +6,164 @@ using System.Text;
 using K4os.Compression.LZ4;
 using Newtonsoft.Json;
 
-string filePath = @"C:\Work\bg3pak\paks\ImpUI_26922ba9-6018-5252-075d-7ff2ba6ed879.pak";
 
-using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-using (BinaryReader br = new BinaryReader(fs))
+string filePath = @"C:\Users\insom\Downloads\5e Spells-125-2-1-0-6-1724104388\5eSpells.pak";
+
+PakFileLoader pakLoader = new PakFileLoader();
+pakLoader.LoadFromFile(filePath);
+
+
+public class PakFileLoader
 {
-    var magic = br.ReadBytes(4);
-
-    var data = new Header
+    public struct Header
     {
-        Version = br.ReadUInt32(),
-        FileListOffset = br.ReadUInt64(),
-        FileListSize = br.ReadUInt32(),
-        Flags = br.ReadByte(),
-        Priority = br.ReadByte(),
-        Md5 = br.ReadBytes(16), // Assuming MD5 is 16 bytes
-        numParts = br.ReadUInt16()
-    };
-
-    ReadCompressedFileList(br, (int)data.FileListOffset);
-
-    // Process the data as needed
-    Console.WriteLine(JsonConvert.SerializeObject(data, Formatting.Indented));
-}
-
-
-void ReadCompressedFileList(BinaryReader br, int offset)
-{
-    br.BaseStream.Seek(offset, SeekOrigin.Begin);
-
-    var numOfFiles = br.ReadInt32();
-    var compressedSize = br.ReadInt32();
-
-    Console.WriteLine(numOfFiles);
-    Console.WriteLine(compressedSize);
-
-    //int fileBufferSize = Marshal.SizeOf(typeof(FileEntry18)) * numOfFiles;
-    int fileBufferSize = compressedSize * 50;
-
-    var compressed = br.ReadBytes((int)compressedSize);
-
-    // Start with a buffer that is large enough
-    byte[] decompressed = new byte[fileBufferSize];
-
-    int decodedBytes = LZ4Codec.Decode(compressed, 0, compressed.Length, decompressed, 0, decompressed.Length);
-
-    Console.WriteLine($"DecodedBytes {decodedBytes}");
-    
-    if (decodedBytes == 0)
-    {
-        throw new InvalidOperationException("Decompression failed.");
+        public uint Version;
+        public ulong FileListOffset;
+        public uint FileListSize;
+        public byte Flags;
+        public byte Priority;
+        public byte[] Md5;
+        public ushort numParts;
     }
 
-    // Resize the buffer to the actual decompressed size
-    Array.Resize(ref decompressed, decodedBytes);
-
-    // Process the uncompressed data as needed
-    Console.WriteLine("Decompression successful.");
-
-    string tempFilePath = @"C:\Work\bg3pak\paks\temp.bin";
-    File.WriteAllBytes(tempFilePath, decompressed);
-    
-    using var ms = new MemoryStream(decompressed);
-    using var msr = new BinaryReader(ms);
-
-    var entries = new List<FileEntry18>();
-    
-    for(var i = 0; i < numOfFiles; i++)
+    public struct FileEntry18
     {
-        var entry = new FileEntry18
+        public string Name;
+        public uint OffsetInFile1;
+        public ushort OffsetInFile2;
+        public byte ArchivePart;
+        public byte Flags;
+        public uint SizeOnDisk;
+        public uint UncompressedSize;
+    }
+    
+    private const string MAGIC_BYTES = "LSPK";
+    private const int FILE_ENTRY_SIZE = 272;
+
+    public void LoadFromFile(string filePath)
+    {
+        using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        using (BinaryReader br = new BinaryReader(fs))
         {
-            Name = Encoding.UTF8.GetString(msr.ReadBytes(256)).TrimEnd('\0'),
-            OffsetInFile1 = msr.ReadUInt32(),
-            OffsetInFile2 = msr.ReadUInt16(),
-            ArchivePart = msr.ReadByte(),
-            Flags = msr.ReadByte(),
-            SizeOnDisk = msr.ReadUInt32(),
-            UncompressedSize = msr.ReadUInt32()
-        };
-        
-        Console.WriteLine(JsonConvert.SerializeObject(entry, Formatting.Indented));
-        
-        entries.Add(entry);
+            Load(br);
+        }
     }
-}
 
-public struct Header
-{
-    public uint Version;
-    public ulong FileListOffset;
-    public uint FileListSize;
-    public byte Flags;
-    public byte Priority;
-    public byte[] Md5;
-    public ushort numParts;
-}
+    public void LoadFromByteArray(byte[] fileData)
+    {
+        using (MemoryStream ms = new MemoryStream(fileData))
+        using (BinaryReader br = new BinaryReader(ms))
+        {
+            Load(br);
+        }
+    }
 
-[StructLayout(LayoutKind.Sequential, Pack = 1)]
-public struct FileEntry18
-{
-    //[MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
-    public string Name;
-    
-    public uint OffsetInFile1;
-    public ushort OffsetInFile2;
-    public byte ArchivePart;
-    public byte Flags;
-    public uint SizeOnDisk;
-    public uint UncompressedSize;
+    private void Load(BinaryReader br)
+    {
+        var magic = br.ReadBytes(4);
+
+        if (Encoding.UTF8.GetString(magic) != MAGIC_BYTES)
+        {
+            throw new Exception($"Not a valid BG3 PAK. Magic bytes ({MAGIC_BYTES}) not found.");
+        }
+
+        var data = new Header
+        {
+            Version = br.ReadUInt32(),
+            FileListOffset = br.ReadUInt64(),
+            FileListSize = br.ReadUInt32(),
+            Flags = br.ReadByte(),
+            Priority = br.ReadByte(),
+            Md5 = br.ReadBytes(16),
+            numParts = br.ReadUInt16()
+        };
+
+        // display header
+        Console.WriteLine(JsonConvert.SerializeObject(data, Formatting.Indented));
+        
+        ReadCompressedFileList(br, (int)data.FileListOffset);
+    }
+
+    private void ReadCompressedFileList(BinaryReader br, int offset)
+    {
+        br.BaseStream.Seek(offset, SeekOrigin.Begin);
+
+        var numOfFiles = br.ReadInt32();
+        var compressedSize = br.ReadInt32();
+
+        Console.WriteLine($"Number of files: {numOfFiles}");
+        Console.WriteLine($"Compressed size: {compressedSize}");
+
+        var decompressedSize = numOfFiles * FILE_ENTRY_SIZE;
+        var compressed = br.ReadBytes(compressedSize);
+
+        byte[] decompressed = new byte[decompressedSize];
+        int decodedBytes = LZ4Codec.Decode(compressed, 0, compressed.Length, decompressed, 0, decompressed.Length);
+
+        Console.WriteLine($"DecodedBytes {decodedBytes}");
+
+        if (decodedBytes == 0)
+        {
+            throw new InvalidOperationException("Decompression failed.");
+        }
+
+        //Array.Resize(ref decompressed, decodedBytes);
+        Console.WriteLine("Decompression successful.");
+
+        // write temp bytes so we can see what we're working with
+        File.WriteAllBytes(@"C:\Work\bg3pak\paks\temp.bin", decompressed);
+
+        // new mem stream from decompress bytes
+        using var ms = new MemoryStream(decompressed);
+        using var msr = new BinaryReader(ms);
+
+        // built up list of file entries
+        var entries = new List<FileEntry18>();
+
+        for (var i = 0; i < numOfFiles; i++)
+        {
+            FileEntry18 entry = new FileEntry18
+            {
+                Name = Encoding.UTF8.GetString(msr.ReadBytes(256)).TrimEnd('\0'),
+                OffsetInFile1 = msr.ReadUInt32(),
+                OffsetInFile2 = msr.ReadUInt16(),
+                ArchivePart = msr.ReadByte(),
+                Flags = msr.ReadByte(),
+                SizeOnDisk = msr.ReadUInt32(),
+                UncompressedSize = msr.ReadUInt32()
+            };
+
+            entries.Add(entry);
+        }
+        
+        // look through file entries for meta.lsx
+        var metaLsx = entries.FirstOrDefault(e => e.Name.Contains("meta.lsx"));
+
+        // if we have something, then read the data
+        if (metaLsx.Name != null)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(metaLsx, Formatting.Indented));
+
+            byte[] metaLsxData = ReadFileEntryData(br, metaLsx, (int)metaLsx.OffsetInFile1, (int)metaLsx.SizeOnDisk);
+
+            string metaLsxFilePath = @"C:\Work\bg3pak\paks\meta.lsx";
+            File.WriteAllBytes(metaLsxFilePath, metaLsxData);
+        }
+        else
+        {
+            Console.WriteLine("meta.lsx not found.");
+        }
+    }
+
+    private byte[] ReadFileEntryData(BinaryReader br, FileEntry18 fileMeta, int offset, int size)
+    {
+        br.BaseStream.Seek(offset, SeekOrigin.Begin);
+
+        var data = br.ReadBytes(size);
+        byte[] decompressed = new byte[fileMeta.UncompressedSize];
+
+        int decodedBytes = LZ4Codec.Decode(data, 0, data.Length, decompressed, 0, decompressed.Length);
+
+        return decompressed;
+    }
 }
