0afb9387bcbee3fa684522f4d44dd2f16d3daec2
insomnious <simon.davies@nexusmods.com>
2024-09-30 08:47:32 +0100

    double checking

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
